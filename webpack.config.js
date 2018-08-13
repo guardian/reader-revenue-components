11bd89d21bbef13ad8f1bbac34c354c3ec6d1407
guy.dawson <guy.dawson@theguardian.com>
2018-08-13 16:26:28 +0100

    commit initial changes - inline javascript and css; html template

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inlineSource: '.(js|css)$',
+            template: './src/index.template.html'
+        }),
+        new ExtractTextPlugin("styles.css"),
+        new HtmlWebpackInlineSourcePlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            }
+        ]
+    }
+};
