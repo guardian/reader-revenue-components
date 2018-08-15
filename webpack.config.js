ad6bf8411c9522bc8ded7ab1c67ad155553c8811
Joseph Smith <joseph.smith@guardian.co.uk>
2018-08-15 20:05:29 +0100

    webpack.config.js to 2 spaces

--- webpack.config.js
+++ webpack.config.js
@@ -6,90 +6,90 @@ const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const path = require('path');
 
 function getPolyfillUrl() {
-    const features = [
-        'Promise',
-        'requestAnimationFrame',
-        'URL',
-    ];
-    return `https://polyfill.guim.co.uk/v2/polyfill.min.js?features=${encodeURIComponent(features.join(','))}`
+  const features = [
+    'Promise',
+    'requestAnimationFrame',
+    'URL',
+  ];
+  return `https://polyfill.guim.co.uk/v2/polyfill.min.js?features=${encodeURIComponent(features.join(','))}`
 }
 
 function getSupportUrl() {
-    const acquisitionData = {
-        componentType: 'ACQUISITIONS_EPIC',
-        source: 'GUARDIAN_WEB',
-        componentId: 'iframe_control_epic_no_js',
-        abTest: {
-            name: 'iframe_or_not',
-            variant: 'iframe'
-        }
-    };
-    return `https://support.theguardian.com?acquisitionData=${encodeURIComponent(JSON.stringify(acquisitionData))}`
+  const acquisitionData = {
+    componentType: 'ACQUISITIONS_EPIC',
+    source: 'GUARDIAN_WEB',
+    componentId: 'iframe_control_epic_no_js',
+    abTest: {
+      name: 'iframe_or_not',
+      variant: 'iframe'
+    }
+  };
+  return `https://support.theguardian.com?acquisitionData=${encodeURIComponent(JSON.stringify(acquisitionData))}`
 }
 
 function buildTemplateParams() {
-    return {
-        supportUrl: getSupportUrl(),
-        polyfillUrl: getPolyfillUrl()
-    }
+  return {
+    supportUrl: getSupportUrl(),
+    polyfillUrl: getPolyfillUrl()
+  }
 }
 
 module.exports = {
-    mode: 'production',
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true,
-                sourceMap: true // set to true if you want JS source maps
-            }),
-            new OptimizeCSSAssetsPlugin({})
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inlineSource: '.(js|css)$',
+      template: './src/index.template.html',
+      minify: true,
+      templateParams: buildTemplateParams(),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new HtmlWebpackInlineSourcePlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
         ]
-    },
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            inlineSource: '.(js|css)$',
-            template: './src/index.template.html',
-            minify: true,
-            templateParams: buildTemplateParams(),
-        }),
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: "[id].css"
-        }),
-        new HtmlWebpackInlineSourcePlugin()
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                MiniCssExtractPlugin.loader,
-                    "css-loader", // translates CSS into CommonJS
-                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
-                ]
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.js$/,
-                loader: "eslint-loader",
-                options: {
-                    // eslint options (if necessary)
-                }
-            }
-        ]
-    }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: "eslint-loader",
+        options: {
+          // eslint options (if necessary)
+        }
+      }
+    ]
+  }
 };
