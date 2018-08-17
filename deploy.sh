fc88f4028bb607fd0d542418372ddcc70dc1b3b1
Joseph Smith <joseph.smith@guardian.co.uk>
2018-08-17 11:50:06 +0100

    Change path to iframe-or-not-test-v2

--- deploy.sh
+++ deploy.sh
@@ -8,17 +8,17 @@ yarn run build
 upload_file () {
     FILE="$1"
     UPLOAD_PATH="$2"
-    
+
     aws s3 cp \
         --profile membership \
         --region eu-west-1 \
         --acl public-read "./dist/${FILE}" "s3://reader-revenue-components/${UPLOAD_PATH}/${FILE}" \
         --cache-control 60 \
         --metadata '{"surrogate-control":"86400"}'
-    
+
     curl -X PURGE "https://support.code.dev-theguardian.com/${UPLOAD_PATH}/${FILE}"
     curl -X PURGE "https://support.theguardian.com/${UPLOAD_PATH}/${FILE}"
 }
 
-upload_file "index.html"                 "epic/iframe-or-not-test"
-upload_file "paypal-and-credit-card.png" "epic/iframe-or-not-test"
+upload_file "index.html"                 "epic/iframe-or-not-test-v2"
+upload_file "paypal-and-credit-card.png" "epic/iframe-or-not-test-v2"
